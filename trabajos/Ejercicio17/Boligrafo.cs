bb73786c20859dfb790e6b7720fd4994b3a35473
alumno <alejandro13frank@hotmail.com>
2019-08-28 22:18:12 -0300

    asd

---
+++ trabajos/Ejercicio17/Boligrafo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio17
+{
+    public class Boligrafo
+    {
+        public const short cantidadTintaMaxima = 100;
+        private ConsoleColor color;
+        private short tinta;
+
+        public Boligrafo(ConsoleColor colorNuevo,short cantTinta)
+        {
+            color = colorNuevo;
+            tinta = cantTinta;
+        }
+        public short GetTinta()
+        {
+            return this.tinta;
+        }
+        public ConsoleColor GetColor()
+        {
+            return this.color;
+        }
+        private void setTinta(short tinta)
+        {
+            if ((tinta + this.tinta) >= 0 && (tinta + this.tinta) <= cantidadTintaMaxima)
+            {
+                this.tinta += tinta;
+            }
+        }
+        public void Recargar()
+        {
+            this.setTinta((short)(cantidadTintaMaxima - this.tinta));
+        }
+        public bool Pintar(short gasto,out string dibujo)
+        {
+            int i;
+            dibujo = "";
+            for (i=gasto;i>0 && this.tinta>0;i--)
+            {
+                dibujo += "*";
+                this.setTinta(-1);
+            }
+            return true;
+        }
+    
+    }
+}
---
+++ trabajos/Ejercicio17/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boligrafo boliAzul = new Boligrafo(ConsoleColor.Blue,100);
+            Boligrafo boliRojo = new Boligrafo(ConsoleColor.Red,50);
+            string dibujo="";
+            boliAzul.Pintar(10, out dibujo);
+            Console.ForegroundColor = boliAzul.GetColor();
+            Console.WriteLine(dibujo);
+            Console.ReadKey();
+
+        }
+    }
+}
