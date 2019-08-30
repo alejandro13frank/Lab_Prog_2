f237eddeca85b1a5af251cb99b25067b65ebdb04
alumno <alejandro13frank@hotmail.com>
2019-08-29 21:57:54 -0300

    hasta19

---
+++ trabajos/Ejercicio19/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sumador s1 = new Sumador(10);
+            Sumador s2 = new Sumador(15);
+            Console.WriteLine(s1 + s2);
+            Console.WriteLine((int)s1);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ trabajos/Ejercicio19/Sumador.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio19
+{
+    class Sumador
+    {
+        private int cantidadSumas;
+
+        public Sumador(int a)
+        {
+            cantidadSumas = a;
+        }
+        public Sumador()
+        {
+            cantidadSumas = 0;
+        }
+        public long Sumar(long a,long b)
+        {
+            this.cantidadSumas++;
+            return a + b;
+        }
+        public string Sumar(string a, string b)
+        {
+            this.cantidadSumas++;
+            return a + b;
+        }
+        public int GetCantidadSumas()
+        {
+            return this.cantidadSumas;
+        }
+        public static explicit operator int(Sumador s1)
+        {
+            return s1.GetCantidadSumas();
+        }
+
+        public static long operator +(Sumador s1,Sumador s2)
+        {
+            return s1.GetCantidadSumas() + s2.GetCantidadSumas();   
+        }
+        public static bool operator |(Sumador s1, Sumador s2)
+        {
+            bool retorno = false;
+            if (s1.GetCantidadSumas()==s2.GetCantidadSumas())
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+    }
+}
