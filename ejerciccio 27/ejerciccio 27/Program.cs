8e47995ba03ae739996b9397fd6cb09427888de1
alumno <alejandro13frank@hotmail.com>
2019-09-12 22:08:35 -0300

    asd

---
+++ ejerciccio 27/ejerciccio 27/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejerciccio_27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random numeroRandom = new Random();
+            int i;
+            List<int> listaNumero = new List<int>();
+            Queue<int> colaNumero = new Queue<int>();
+            Stack<int> pilaNumero = new Stack<int>();
+            for (i=0;i<20;i++)
+            {
+                listaNumero.Add(numeroRandom.Next(-100, 100));
+            }
+
+            foreach (int numero in listaNumero)
+            {
+                Console.WriteLine(numero);
+            }
+            Console.ReadKey();
+            Console.WriteLine("--------Ordenados-----------");
+
+            listaNumero.Sort();
+            foreach (int numero in listaNumero)
+            {
+                Console.WriteLine(numero);
+            }
+            Console.ReadKey();
+            Console.WriteLine("--------positivos-----------");
+            foreach (int numero in listaNumero)
+            {
+                if (numero<0)
+                {
+                    Console.WriteLine("encole negativo {0}", numero);
+                    colaNumero.Enqueue(numero);
+                }
+                else if (numero!=0)
+                {
+                    Console.WriteLine("apile pisitivo {0}", numero);
+                    pilaNumero.Push(numero);
+                }
+            }
+            Console.WriteLine("negativos ascendentes------------");
+            while (colaNumero.Count > 0)
+            {
+                Console.WriteLine(colaNumero.Dequeue());
+            }
+            Console.WriteLine("positivos descendentes------------");
+            while (pilaNumero.Count > 0)
+            {
+                Console.WriteLine(pilaNumero.Pop());
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ ejercicio 28/ejercicio 28/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio_28
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static int compararKey(KeyValuePair<string,int> a, KeyValuePair<string, int> b)
+        {
+            return -a.Value.CompareTo(b.Value);
+        }
+
+        private void btnCalcularPalabras(object sender, EventArgs e)
+        {
+            Dictionary<string, int> contadorPalabras = new Dictionary<string, int>();
+            string[] arrayDeString;
+            char[] arrayDeChar = {' ',',',';','.',':','\n','\t'};
+            arrayDeString = rtxtTexto.Text.Split(arrayDeChar);
+            for (int i = 0; i < arrayDeString.Length; i++)
+            {
+                if (arrayDeString[i] != "")
+                {
+                    try
+                    {
+                        contadorPalabras.Add(arrayDeString[i], 1);
+                    }
+                    catch (ArgumentException)
+                    {
+                        contadorPalabras[arrayDeString[i]]++;
+                    }
+                }
+                
+            }
+           foreach (KeyValuePair<string, int> palabra in contadorPalabras)
+            {
+                MessageBox.Show($"Palabra : {palabra.Key}\n repeticiones: {palabra.Value}");
+            }
+            List<KeyValuePair<string, int>> auxLista = contadorPalabras.ToList();
+
+            auxLista.Sort(compararKey);
+
+            foreach (KeyValuePair<string, int> palabra in auxLista)
+            {
+                MessageBox.Show($"palabra: {palabra.Key}\n  repeticiones: {palabra.Value}");
+            } 
+        }
+    }
+}
