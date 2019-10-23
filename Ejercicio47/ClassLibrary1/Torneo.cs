2d9f7446e85ad76b8720b9849a53dfa1fbc583d0
alumno <alejandro13frank@hotmail.com>
2019-10-23 19:23:24 -0300

    asd

---
+++ Ejercicio47/ClassLibrary1/Torneo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Torneo<T> where T:Equipo
+    {
+        List<T> equipos;
+        string nombre;
+
+        public static bool operator ==(Torneo<T> torneo, Equipo equipo)
+        {
+            foreach (var e in torneo.equipos)
+            {
+                if (e==equipo)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool operator !=(Torneo<T> torneo, Equipo equipo)
+        {
+            return !(torneo.equipos == equipo);
+        }
+    }
+}
