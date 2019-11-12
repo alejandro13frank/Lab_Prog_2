9ebe028c8f8ab12761a48a54aeabf699cf8ae422
alumno <alumno@utnfra.com.ar>
2019-11-12 20:14:41 -0300

    asd

---
+++ Ejercicio67/FrmTimer/FrmTimer/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace FrmTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHora = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnBoton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblHora
+            // 
+            this.lblHora.AutoSize = true;
+            this.lblHora.Location = new System.Drawing.Point(25, 23);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(35, 13);
+            this.lblHora.TabIndex = 0;
+            this.lblHora.Text = "label1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 56);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(625, 244);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // btnBoton
+            // 
+            this.btnBoton.Location = new System.Drawing.Point(358, 18);
+            this.btnBoton.Name = "btnBoton";
+            this.btnBoton.Size = new System.Drawing.Size(75, 23);
+            this.btnBoton.TabIndex = 2;
+            this.btnBoton.Text = "button1";
+            this.btnBoton.UseVisualStyleBackColor = true;
+            this.btnBoton.Click += new System.EventHandler(this.btnBoton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 312);
+            this.Controls.Add(this.btnBoton);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.lblHora);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHora;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnBoton;
+    }
+}
+
---
+++ Ejercicio67/FrmTimer/FrmTimer/Timer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace FrmTimer
+{
+    public delegate void EncargadoTiempo();
+    public class Timer
+    {
+        Thread hilo;
+        int intervalo;
+        public event EncargadoTiempo eventoTiempo; 
+        bool activo;
+        public Timer()
+        {
+            hilo = new Thread(this.Corriendo);
+            this.Intervalo = 1000;
+        }
+        public bool Activo
+        {
+            get
+            {
+                return this.activo;
+            }
+            set
+            {
+                if (value)
+                {
+                    this.activo = value;
+                    this.hilo.Start();
+                }
+                else
+                {
+                    this.activo = value;
+                    this.hilo.Abort();
+                }
+            }
+        }
+        public int Intervalo
+        {
+            get
+            {
+                return this.intervalo;
+            }
+            set
+            {
+                this.intervalo = value;
+            }
+        }
+        private void Corriendo()
+        {
+            do
+            {
+               System.Threading.Thread.Sleep(this.Intervalo);
+               this.eventoTiempo.Invoke();
+            } while (true);
+        }
+
+    }
+}
